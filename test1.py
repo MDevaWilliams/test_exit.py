ce37ef8021de7a9a51c22d217d19a3350c28f597
MDevaWilliams <107609876+MDevaWilliams@users.noreply.github.com>
2024-10-23 11:06:39 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,13 +1,15 @@
-!/bin/bash
+#!/bin/bash
 set -xe
-Update packages
+
+# Update packages
 apt-get update
-Install required packages
+
+# Install required packages
 apt-get install -y python3 python3-pip libpq-dev python3-dev python3.11-venv
+
 # Create a virtual environment to manage Python packages
 python3 -m venv venv
 source venv/bin/activate
-Install the psycopg2 module in the virtual environment
+
+# Install the psycopg2 module in the virtual environment
 pip install psycopg2-binary
-
-
